cdf03fac49a9b15d6b338bed40eeb55012578c48
Sahid-Al-Masud-Aayan <aayan.webdeveloper@gmail.com>
2025-03-16 19:25:26 +0600

    latest

--- client/socket/src/App.jsx
+++ client/socket/src/App.jsx
@@ -1,21 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import io from 'socket.io-client'; 
 const socket = io.connect('http://localhost:3000');
 
 function App() {
 
+  const [message, SetMessage]= useState("")
+  const handleText=(e)=>{
+    SetMessage(e.target.value)
+  }
+  const sender=()=>{
+    socket.emit("clients-message",{
+      message
+    })
+    
+  }
   useEffect(()=>{
     socket.emit('message-from-frontend', {
       message: 'hello I am aayan'
+    })
+    socket.on('read-data-from-db', (info)=>{
+      console.log(info);
+      
     })
   })
   socket.on('message-from-server', (data)=>{
     console.log(data);
     
 })
-
+   
   return (
+    <>
     <h1>Hello I am connecting the socket</h1>
+    <input onChange={handleText} type="text" />
+    <button onClick={sender}>send</button>
+    </>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const app = express()
 const cors = require('cors');
 app.use(cors());
 const { Server } = require("socket.io");
+const mongoose = require('mongoose');
+
+// enFVCx0CJTb8Qj8s
+
+mongoose.connect('mongodb+srv://Socket:enFVCx0CJTb8Qj8s@cluster0.uszpu.mongodb.net/Socketdata?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('mongo db Connected!'));
+
 
 const server = app.listen(3000, (req, res)=>{
     console.log('server on port 3000');
@@ -11,14 +18,34 @@ const server = app.listen(3000, (req, res)=>{
 const io = new Server(server, {
     cors: '*',
   });
+  
+  const Schema = mongoose.Schema;
 
+const SocketText = new Schema({
+  text: String,
+});
+
+const SocketTextModel = mongoose.model('socketInfo', SocketText)
   io.on('connection', (socket)=>{
     console.log('backend connected with frontend', socket.id);
-    socket.on('message-from-frontend', (data)=>{
-        console.log(data);
-        
+    socket.on('clients-message', async (data)=>{
+      const SocketdataText = await SocketTextModel({
+        text: data.message
+      })
+      SocketdataText.save()
+        io.emit('ans_from_server', data)
     })
     io.emit('message-from-server', {
         message:'the signal is from server'
+    })
+    socket.on('read-data-from-db', async ()=>{
+      const TetxData = await SocketTextModel.find()
+      socket.emit("read", {
+        message:TetxData
+      })
+      
+    })
+    io.on('disconnected', ()=>{
+        console.log('disconnected');
     })
   })
